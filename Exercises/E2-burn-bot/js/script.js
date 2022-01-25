8e53cfb6f23dec95bcda9ff34095c2bf2ad7db96
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-01-24 21:11:28 -0500

    E2 : started exercise, jotted to do list in top comments

---
+++ Exercises/E2-burn-bot/js/script.js
@@ -0,0 +1,43 @@
+/**
+Burn This Bot
+Frankie Latreille
+
+The bot will prompt the user with yes or no questions that will be registered through annyang
+(or typed to get over non chrome users), and answers with responsiveVoice with more
+yes or no questions
+*/
+
+/**
+to do:
+- make bot class with 3 faces (offline, listening, talking)
+- setup aanyang to receive "yes" "no" or "I don't" ('get it' or 'know' or 'understand')
+- setup responsiveVoice arrays for yesTangents and noTangents (SCRIPTING)
+- have start state, middle state, after 3 responses (goodbye state?)
+- have "I don't *" particular response higher pitch, red screen "Ooo I don't *whatever user said"
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
