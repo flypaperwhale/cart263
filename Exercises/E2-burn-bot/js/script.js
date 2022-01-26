73c0962a9a9e959eb51f24a05c2d44d346234439
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-01-25 21:05:25 -0500

    E2 : annyand typo

--- Exercises/E2-burn-bot/js/script.js
+++ Exercises/E2-burn-bot/js/script.js
@@ -130,7 +130,7 @@ function onlineState() {
         "that's *insult": botInsult,
       };
       annyang.addCommands(commands);
-      annyand.debug();
+      annyang.debug();
     }
 
     if (bot.turnsSpoken === 1) {
