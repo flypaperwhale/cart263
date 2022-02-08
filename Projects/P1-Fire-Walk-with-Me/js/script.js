bcb0e17c9f88c3747c16a047b6817f010ed0662e
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-02-08 18:57:36 -0500

    P1 : copied dialog code, started scripting
    while I script, adding ways functions should work or what functions i will need to implement

--- Projects/P1-Fire-Walk-with-Me/js/script.js
+++ Projects/P1-Fire-Walk-with-Me/js/script.js
@@ -1,33 +1,130 @@
 /**
-Title of Project
-Author Name
+Fire Walk with Me
+Frankie Latreille
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Reimagined scenes of David Lynch's Fire Walk with Me. Laura meets a trucker who wants
+to pay her for sex work, she is sent into her subconscious where a woman tries to warn
+her, and then continues her decent into Hell
 */
 
 "use strict";
 
+// /**
+// Description of preload
+// */
+// function preload() {}
+//
+// /**
+// Description of setup
+// */
+// function setup() {}
+//
+// /**
+// Description of draw()
+// */
+// function draw() {}
+
+// To store the loaded data
+let data = undefined;
+// The current scene (there's only one in the data, but this would be how you display different scenes)
+let currentScene = `scene1`;
+// The current line in the current scene (going through an array of dialog, so starts at 0)
+let currentLine = 0;
+// The height of our dialog box
+let dialogHeight = 100;
 
 /**
-Description of preload
+Loads the JSON data for our little play
 */
 function preload() {
-
+  data = loadJSON(`assets/data/dialog.json`);
 }
 
-
 /**
-Description of setup
+Creates the canvas
 */
 function setup() {
-
+  createCanvas(320, 240);
 }
 
-
 /**
-Description of draw()
+Displays the current line
 */
 function draw() {
+  background(0);
 
+  displayCurrentLine();
+}
+
+/**
+Uses the dialog and character data to display a dialog box
+colored and labelled by character and with the current line
+displayed in it.
+*/
+function displayCurrentLine() {
+  // Get the current scene and line data object
+  // NOTE: Notice how we can use *variables* to choose
+  // a property in an object like data.dialog! This gives
+  // us a lot of flexibility because we can store the property
+  // name we want in a variable and change it (like the current
+  // scene name!)
+  let lineData = data.dialog[currentScene][currentLine];
+  // Get the data for the character who is speaking, note
+  // we're using the same trick of a variable containing the
+  // property name corresponding to our character
+  let characterData = data.characters[lineData.character];
+
+  // Draw the dialog background
+  push();
+  noStroke();
+  //fill(characterData.backgroundColor);
+  fill(100, 50, 0);
+  rect(0, height - dialogHeight, width, dialogHeight);
+  pop();
+
+  // Display the character name
+  push();
+  textSize(24);
+  //fill(characterData.backgroundColor);
+  fill(100, 50, 0);
+  textAlign(LEFT, BOTTOM);
+  text(characterData.name, 0, height - dialogHeight);
+  pop();
+
+  // Display the character's line
+  push();
+  textSize(16);
+  fill(0);
+  textAlign(LEFT, TOP);
+  text(lineData.speech, 0, height - dialogHeight, width, dialogHeight);
+  pop();
+}
+
+/**
+Goes to the next line in the "play"
+*/
+function mousePressed() {
+  nextLine();
+}
+
+/**
+Iterates through the array, returns to 0 at the end, but you
+would probably want to change to the next scene etc.
+*/
+function nextLine() {
+  currentLine++;
+  if (currentLine === data.dialog[currentScene].length) {
+    currentLine = 0;
+  }
+}
+
+/**
+Switch to scene two on a keypress, just to show the very
+beginnings of multiple scenes.
+*/
+/* ##### Scenes are going to change after the user speaks a response
+when the last dialog piece has been said*/
+function keyPressed() {
+  currentScene = `scene2`;
+  currentLine = 0;
 }
