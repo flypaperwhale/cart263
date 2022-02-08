aebd5305965bc837b05715a2e89b3195fffe1c3a
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-02-07 21:47:38 -0500

    Activity Bubble Popper half way

--- Activities/A-Bubble-Popper/js/script.js
+++ Activities/A-Bubble-Popper/js/script.js
@@ -1,33 +1,39 @@
 /**
-Title of Project
-Author Name
+Bubble Popper
+Frankie Latreille
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Pop bubbles with your finger as a pin. (building off Pippin's code)
 */
 
 "use strict";
 
+// user's webcam
+let video = undefined;
+// the handpose model
+let handpose = undefined;
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(640, 480);
 
+  // access user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // load handpose model
+  handpose = ml5.handpose(video, { flipHorizontal: true }, function () {
+    console.log(`model loaded`);
+  });
 }
-
 
 /**
 Description of draw()
 */
-function draw() {
-
-}
+function draw() {}
