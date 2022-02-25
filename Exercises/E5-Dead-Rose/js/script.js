9525e60c1a3fe01e82b3a316b7db35e8295ca66e
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-02-24 22:45:43 -0500

    E5 : have three images sized and loaded through html
    need to adjust their positions with css

---
+++ Exercises/E5-Dead-Rose/js/script.js
@@ -0,0 +1,25 @@
+// fill variables according to png clicked
+// replace variables in poem with random from particular categories
+// change html "body" with the poem, revealing one verse at a time?
+
+let thePoemDisplayed = `
+O Rose thou art sick.
+The invisible worm,
+That flies in the night
+In the howling storm:
+
+Has found out thy bed
+Of crimson joy:
+And his dark secret love
+Does thy life destroy.`;
+
+/*`
+O Rose thou art sick.
+The invisible worm,
+That flies in the night
+In the howling storm:
+
+Has found out thy bed
+Of crimson joy:
+And his dark secret love
+Does thy life destroy.`*/
