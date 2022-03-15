560882403f5404981d869f2cfe2f6bec4c16083a
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-03-15 17:05:19 -0400

    P2 Prototype: started, added image assets, drew background

---
+++ Projects/P2-versions/P2-prototype/js/script.js
@@ -0,0 +1,56 @@
+/**
+Mish Mash
+Frankie Latreille
+
+This is the prototype for my CART 263 P2
+*/
+
+/*
+TO DO:
++ use OOP states // title, simulation (would possibly be subdivided in final),
++ create canvas (grass and sky)
++ implement a grid system
++ create Player class
++ create NPC class
++ create Object class
++ have player pick up item, add item to an inventory,
++ have player select item box or empty box, selected box display item over player
++ have player give item to NPC (this will manipulate NPC property value for event "collecting item X")
++ have possibility for player to collect multiples of item
++ have item disappear when picked, and a new one appear randomly on map
+
+*/
+
+let peachImage, peachTreeImage;
+
+("use strict");
+
+/**
+Description of preload
+*/
+function preload() {
+  // image assets
+  peachImage = loadImage(`assets/images/peach.png`);
+  peachTreeImage = loadImage(`assets/images/peachtree.png`);
+}
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(500, 600);
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+  noStroke();
+  background(`skyblue`);
+  // draw grass
+  push();
+  fill(`green`);
+  rectMode(CENTER);
+  rect(250, 450, width, height / 1.5);
+  pop();
+}
