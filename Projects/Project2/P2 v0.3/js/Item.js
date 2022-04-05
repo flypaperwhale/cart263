a6cc126e3c618858e2f7a6e707d0589516e03681
flypaperwhale <90227256+flypaperwhale@users.noreply.github.com>
2022-04-05 16:22:49 -0400

    P2 : NPC and Item classes created

---
+++ Projects/Project2/P2 v0.3/js/Item.js
@@ -0,0 +1,10 @@
+class Item {
+  constructor(){
+    this.name = undefined;
+    this.cellLabel = undefined;
+    this.type = undefined;
+    this.value = undefined;
+    this.imageName = undefined;
+    this.dropZone = []; // used unless item is dropped by NPC
+  }
+}
---
+++ Projects/Project2/P2 v0.3/js/NPC.js
@@ -0,0 +1,19 @@
+class NPC {
+  constructor() {
+    this.name = undefined;
+    this.index = {
+      row: undefined,
+      collumn: undefined,
+    };
+    this.cellLabel = undefined;
+    this.color = undefined;
+    this.personality = undefined;
+    this.playerRelation = 0;
+    this.itemRelations = {
+      peach: 0,
+    };
+    this.itemDropZone = [];
+  }
+
+  // events
+}
